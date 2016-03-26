1a893669c0273872878957a74f7c65a3d436a4c2
glassysky <873661157@qq.com>
2016-03-26 08:01:11 +0800

    ng2 example

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,19 @@
 import {Component} from 'angular2/core';
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
-export class AppComponent { }
+
+export class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
