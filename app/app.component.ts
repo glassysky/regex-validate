c791d9ff4c4bde9697aac148e2fc534219af6e0e
glassysky <873661157@qq.com>
2016-03-26 10:59:08 +0800

    add select method

--- app/app.component.ts
+++ app/app.component.ts
@@ -24,18 +24,23 @@ var HEROES: Hero[] = [
         <h1>{{title}}</h1>
         <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="#hero of heroes">
-            <!-- each hero goes here -->
-            <span class="badge">{{hero.id}}</span> {{hero.name}}
+            <li *ngFor="#hero of heroes" 
+                [class.selected]="hero === selectedHero"
+                (click)="onSelect(hero)">
+                <!-- each hero goes here -->
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>
-
-        <!--<h2>{{hero.name}} details!</h2>-->
-        <!--<div><label>id: </label>{{hero.id}}</div>-->
-        <!--<div>-->
-            <!--<label>name: </label>-->
-            <!--<input [(ngModel)]="hero.name" placeholder="name">-->
-        <!--</div>-->
+        
+        <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} details!</h2>
+            <div><label>id: </label>{{selectedHero.id}}</div>
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+            </div>
+        </div>
+        
         `,
     styles:[`
         .selected {
@@ -92,8 +97,6 @@ var HEROES: Hero[] = [
 export class AppComponent {
     public heroes = HEROES;
     public title = 'Tour of Heroes';
-    public hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    selectedHero: Hero;
+    onSelect(hero: Hero) { this.selectedHero = hero; }
 }
